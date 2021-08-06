88bf4cb2f233d23ba0ea626d5f19c7a1eb07724b
Xuan Thien Nguyen <huyhoang9a.bk@gmail.com>
2021-08-06 17:40:13 +0700

    w3

---
+++ main.js
@@ -0,0 +1,8 @@
+
+var fullName = 'Chào mừng đến với web của Huy Hoàng Man';
+var run = 'đi thôi nào! ^.^'
+alert(fullName); // output in ra màn hình
+alert(run);
+confirm('Cấm trẻ em dưới 18 tuổi');
+prompt('Nhập tuổi của bạn');
+
